f51561828c84d3492491dc6dceeee668b45fee19
김민기 <kiiimiiin@naver.com>
2023-07-11 19:36:30 +0900

    Initial commit

---
+++ app/src/main/java/com/min/android/solartv/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.min.android.solartv
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.min.android.solartv.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
